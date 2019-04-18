2d60eb7973d7bce4d2eee5818c50eb9c2b703fc0
Michael Clayton <mcclayton@users.noreply.github.com>
2019-04-18 16:27:00 -0700

    Use Vanilla Webpack (#3)
    * Remove create-react-app and use vanilla webpack
    
    * Accidental import
    
    * v0.0.38
    
    * Fix deps
    
    * v0.0.41
    
    * v0.0.42
    
    * v0.0.43
    
    * v0.0.44
    
    * v0.0.2
    
    * v0.0.4
    
    * v0.0.15
    
    * v0.0.16
    
    * v0.0.17
    
    * v0.0.20
    
    * v0.0.22
    
    * Add react as webpack external, babel finessing
    
    * Merge in master
    
    * Remove linting option from testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export { default } from './useFetch'
+export * from './useFetch'
+export * from './useGet'
+export * from './usePost'
+export * from './usePut'
+export * from './usePatch'
+export * from './useDelete'
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    library: 'index',
+    libraryTarget: 'umd',
+    filename: 'index.js',
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+  }
+};
