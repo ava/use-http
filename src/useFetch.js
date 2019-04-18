e6677c2e91dd6a8977606dd200dc8d82e4fd61fe
Michael Clayton <michael.clayton@procore.com>
2019-04-17 20:14:40 -0700

    Accidental import

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 export function useFetch(url, options) {
   // if on server, return loading
