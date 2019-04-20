32765f5f7b98480114ca0b4cafac7fb2c29e335f
Alex Cory <alexcory@fb.com>
2019-04-20 11:40:44 -0700

    fixing bug where arg1 is url and arg2 is options

--- src/useFetch.js
+++ src/useFetch.js
@@ -9,65 +9,67 @@ export function useFetch(arg1, arg2) {
   let baseUrl = ''
   let method = 'GET'
 
-  if (typeof arg1 === 'string') {
-    url = arg1
-    if (isObject(arg2)) options = arg2
-  } else if (isObject(arg1)) {
-    const fetchObj = arg1
+  const handleOptions = opts => {
     if (true) {
       // take out all the things that are not normal `fetch` options
       // need to take this out of scope so can set the variables below correctly
-      let { url, onMount, timeout, baseUrl, ...rest } = fetchObj
+      let { url, onMount, timeout, baseUrl, ...rest } = opts
       options = rest
     }
-    if (fetchObj.url) url = fetchObj.url
-    if (fetchObj.onMount) onMount = fetchObj.onMount
-    if (fetchObj.method) method = fetchObj.method
-    if (fetchObj.baseUrl) baseUrl = fetchObj.baseUrl
+    if (opts.url) url = opts.url
+    if (opts.onMount) onMount = opts.onMount
+    if (opts.method) method = opts.method
+    if (opts.baseUrl) baseUrl = opts.baseUrl
+  }
+
+  if (typeof arg1 === 'string') {
+    url = arg1
+    if (isObject(arg2)) handleOptions(arg2)
+  } else if (isObject(arg1)) {
+    handleOptions(arg1)
   }
 
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(onMount)
   const [error, setError] = useState(null)
 
-  const fetchData = useCallback(method => async (fArg1, fArg2) => {
-    let query = ''
-    const fetchOptions = {}
-    if (isObject(fArg1) && method !== 'GET') {
-      fetchOptions.body = JSON.stringify(fArg1)
-    } else if (baseUrl && typeof fArg1 === 'string') {
-      url = baseUrl + fArg1
-      if (isObject(fArg2)) fetchOptions.body = JSON.stringify(fArg2)
-    }
-    if (typeof fArg1 === 'string' && typeof fArg2 === 'string') {
-      const base = fArg1
-      query = fArg2
-      url = base + query
-    }
-
-    try {
-      setLoading(true)
-      const response = await fetch(url + query, {
-        method,
-        ...options,
-        ...fetchOptions,
-      })
-      let data = null
-      try {
-        data = await response.json()
-      } catch (err) {
-        try {
-          data = await response.text()
-        } catch (err) {
-          setError(`Currently only supports JSON and Text response types: ${err}`)
-        }
+  const fetchData = useCallback(
+    method => async (fArg1, fArg2) => {
+      let query = ''
+      const fetchOptions = {}
+      if (isObject(fArg1) && method !== 'GET') {
+        fetchOptions.body = JSON.stringify(fArg1)
+      } else if (baseUrl && typeof fArg1 === 'string') {
+        url = baseUrl + fArg1
+        if (isObject(fArg2)) fetchOptions.body = JSON.stringify(fArg2)
       }
-      setData(data)
-      setLoading(false)
-    } catch (err) {
-      setError(err)
-    }
-  }, [url])
+      if (typeof fArg1 === 'string' && typeof fArg2 === 'string') query = fArg2
+
+      try {
+        setLoading(true)
+        const response = await fetch(url + query, {
+          method,
+          ...options,
+          ...fetchOptions
+        })
+        let data = null
+        try {
+          data = await response.json()
+        } catch (err) {
+          try {
+            data = await response.text()
+          } catch (err) {
+            setError(`Currently only supports JSON and Text response types: ${err}`)
+          }
+        }
+        setData(data)
+        setLoading(false)
+      } catch (err) {
+        setError(err)
+      }
+    },
+    [url]
+  )
 
   const get = useCallback(fetchData('GET'))
   const post = useCallback(fetchData('POST'))
@@ -79,12 +81,9 @@ export function useFetch(arg1, arg2) {
 
   useEffect(() => {
     if (onMount) request[method.toLowerCase()]()
-  }, [fetchData])
+  }, [])
 
-  return Object.assign(
-    [ data, loading, error, request ],
-    { data, loading, error, request, ...request }
-  )
+  return Object.assign([data, loading, error, request], { data, loading, error, request, ...request })
 }
 
 export default useFetch
