55b7f256c0dce5af1416920b4488070d48e53186
Alex Cory <alexcory@fb.com>
2019-06-27 10:31:53 -0700

    WIP, but a good start

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,20 +1,109 @@
 import { useEffect, useState, useCallback, useRef, useContext, useMemo } from 'react'
 import FetchContext from './FetchContext'
-import { HTTPMethod, Options, UseFetch, FetchCommands, DestructuringCommands, UseFetchResult, useFetchArg1 } from "./types"
-import { invariant, isObject } from './utils'
+import { HTTPMethod, /* Options, */ UseFetch, FetchCommands, DestructuringCommands, UseFetchResult, useFetchArg1 } from "./types"
+import { invariant, isObject, isString, pullOutRequestInit } from './utils'
 
+type OptionsNoURLs = {
+  onMount?: boolean,
+  timeout?: number
+} & RequestInit
 
-export function useFetch<TData = any>(arg1?: useFetchArg1, arg2?: Options | RequestInit): UseFetch<TData> {
+// No Provider
+type URLRequiredOptions = {
+  url: string,
+  onMount?: boolean,
+  timeout?: number,
+} & RequestInit
+
+type BaseURLRequiredOptions = {
+  baseUrl: string
+  onMount?: boolean,
+  timeout?: number,
+} & RequestInit
+
+type OptionsAsFirstParam = URLRequiredOptions | BaseURLRequiredOptions
+
+// With Provider
+type MaybeURLOptions = {
+  url?: string,
+  onMount?: boolean,
+  timeout?: number,
+} & RequestInit
+
+type MaybeBaseURLOptions = {
+  baseUrl?: string
+  onMount?: boolean,
+  timeout?: number,
+} & RequestInit
+
+type MaybeOptions = MaybeURLOptions | MaybeBaseURLOptions
+
+// TODO: this is still yet to be implemented
+type OptionsOverwriteWithContext = (options: MaybeOptions) => MaybeOptions
+
+type OptionsAsFirstParamWithContext = MaybeOptions | OptionsOverwriteWithContext
+
+// Putting it all together
+type URLOrOptions = string | OptionsAsFirstParam | OptionsAsFirstParamWithContext
+
+type Options = OptionsAsFirstParam | OptionsAsFirstParamWithContext | OptionsNoURLs
+
+// No <Provider url='example.com' />
+export function useFetch<TData = any>(url: string, options?: OptionsNoURLs): UseFetch<TData>
+export function useFetch<TData = any>(options: OptionsAsFirstParam): UseFetch<TData>
+// With <Provider url='example.com' />
+// options should be extended. In future maybe have options callback to completely overwrite options
+// i.e. useFetch('ex.com', oldOptions => ({ ...newOptions })) to overwrite
+export function useFetch<TData = any>(url?: string, options?: OptionsNoURLs): UseFetch<TData>
+export function useFetch<TData = any>(options?: OptionsAsFirstParamWithContext): UseFetch<TData>
+
+export function useFetch<TData = any>(urlOrOptions?: URLOrOptions, optionsNoURLs?: OptionsNoURLs): UseFetch<TData> {
   const context = useContext(FetchContext)
 
-  invariant(!!arg1 && !!context.url, 'The first argument of useFetch is required unless you have a global url setup like: <Provider url="https://example.com"></Provider>')
+  // TODO: this needs to be per initial setup below since we need to check urlOrOptions.url OR  urlOrOptions.baseUrl
+  invariant(!!urlOrOptions && !!context.url, 'The first argument of useFetch is required unless you have a global url setup like: <Provider url="https://example.com"></Provider>')
 
-  let url: string | null = context.url || null
-  let options = {} as { signal?: AbortSignal | null } & RequestInit
-  let onMount = false
-  let baseUrl = ''
-  let method: string = HTTPMethod.GET
+  let url: string = ''
+  let options: RequestInit = {}
+  let onMount: boolean = false
+  let timeout: number = 10 // TODO: not implemented
+  let baseUrl: string = ''
+  let method: HTTPMethod = HTTPMethod.GET
 
+  const handleUseFetchOptions = useCallback((useFetchOptions?: UseFetchOptions): void => {
+    const opts = useFetchOptions || {}
+    if (opts.onMount) onMount = opts.onMount
+    if (opts.timeout) timeout = opts.timeout
+    if (opts.baseURL) baseUrl = opts.baseURL
+    if (opts.url) url = opts.url
+  }, [])
+
+  // arg1 = url AND arg2 = options
+  if (isString(urlOrOptions) && isObject(optionsNoURLs)) {
+    url = urlOrOptions as string
+    options = pullOutRequestInit(optionsNoURLs)
+    // currenlty this should only set onMount or timeout
+    handleUseFetchOptions(optionsNoURLs)
+  // arg1 = url AND arg2 = undefined
+  } else if (isString(urlOrOptions) && optionsNoURLs === undefined) {
+    url = urlOrOptions as string
+  // arg1 = options with baseURL and no URL
+  // arg1 = options with URL and no baseURL
+  } else if (isObject(urlOrOptions)) {
+    invariant(!optionsNoURLs, 'You cannot have a 2nd parameter of useFetch when your first argument is a object config.')
+    // I think the types should handle if a `url` and a `baseURL` are not set, TODO: make test for this
+    // I also think it should handle if no `url` and no `baseURL` are set. TODO: make test for this
+    // note on these^ could check with an invariant for both cases in `handleUseFetchOptions`
+    options = pullOutRequestInit(urlOrOptions)
+    handleUseFetchOptions(urlOrOptions)
+  }
+  // Provider: arg1 = undefined
+  // Provider: arg1 = url (overwrites global url) AND arg2 = options (extend global options)
+  // Provider: arg1 = url (overwrites global url) AND arg2 = undefined
+  // Provider: arg1 = options (updates global options) - overwrites URL and no baseURL
+  // Provider: arg1 = options (updates global options) - overwrites baseURL and no URL
+  // Provider: arg1 = options (updates global options) - overwrites any other field
+  // TODO - Provider: arg1 = oldGlobalOptions => ({ my: 'new local options'}) (overwrite all global options for this instance of useFetch)
   const handleOptions = useCallback((opts: Options & RequestInit) => {
     if (true) {
       // take out all the things that are not normal `fetch` options
--- src/utils.ts
+++ src/utils.ts
@@ -19,6 +19,36 @@ export const useExampleURL = (): string => {
   return useMemo(() => isBrowser ? window.location.origin : 'https://example.com', [isBrowser])
 }
 
+export const isString = (x: any) => typeof x === 'string'
+
+// TODO: come back and fix the "anys" in this http://bit.ly/2Lm3OLi
+/**
+ * Makes an object that will match the standards of a normal fetch's options
+ * aka: pulls out all useFetch's special options like "onMount"
+ */
+export const pullOutRequestInit = (options?: {}): RequestInit => {
+  let opts: any = {}
+  if (options) opts = options
+  const requestInitFields = [
+    'body',
+    'cache',
+    'credentials',
+    'headers',
+    'integrity',
+    'keepalive',
+    'method',
+    'mode',
+    'redirect',
+    'referrer',
+    'referrerPolicy',
+    'signal',
+    'window',
+  ]
+  return requestInitFields.reduce((acc: any, key) => {
+    if (key in opts) acc[key] = opts[key]
+    return acc
+  }, {})
+}
 
 /**
  * Determines if the given param is an object. {}
