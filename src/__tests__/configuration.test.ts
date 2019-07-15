d831e050346399791948960c3ecc7877876110ec
Paul Cowan <paul.cowan@bjss.com>
2019-07-15 14:57:22 +0100

    first passing configuration test

--- src/__tests__/configuration.test.ts
+++ src/__tests__/configuration.test.ts
@@ -1,5 +1,10 @@
-describe('use-http configuration', () => {
+import { makeConfig } from "../makeConfig";
+import { renderHook } from '@testing-library/react-hooks';
+
+describe.only('use-http configuration', () => {
   it('should create a default configuration with no arguments', () => {
-    
+    const config = renderHook(() => makeConfig({}, "http://blah.com"))
+
+    expect(config.result.current).toEqual({ onMount: false, url: "http://blah.com" })
   });
 });
