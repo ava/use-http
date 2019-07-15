a4369b1e4832603a36fea2420bdbec7acf0cfb41
Paul Cowan <paul.cowan@bjss.com>
2019-07-15 17:19:55 +0100

    remove renderHook import

--- src/__tests__/configuration.test.ts
+++ src/__tests__/configuration.test.ts
@@ -1,37 +1,36 @@
 import { makeConfig } from "../makeConfig";
-import { renderHook } from '@testing-library/react-hooks';
 
 
 describe.only('use-http configuration', () => {
   it('should create config from a single url argument', () => {
-    const config = renderHook(() => makeConfig({}, "http://blah.com"))
+    const config = makeConfig({}, "http://blah.com")
 
-    expect(config.result.current).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
+    expect(config).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
   });
 
   it('should create a config object from an options object', () => {
-    const config = renderHook(() => makeConfig({}, { url: "http://blah.com" }))
+    const config = makeConfig({}, { url: "http://blah.com" })
 
-    expect(config.result.current).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
+    expect(config).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
   })
 
   it('should set onMount true', () => {
-    const config1 = renderHook(() => makeConfig({}, "http://blah.com", { onMount: true }))
+    const config1 = makeConfig({}, "http://blah.com", { onMount: true })
 
-    expect(config1.result.current).toEqual({ onMount: true, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
+    expect(config1).toEqual({ onMount: true, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
 
-    const config2 = renderHook(() => makeConfig({}, { url: "http://blah.com", onMount: true }))
+    const config2 = makeConfig({}, { url: "http://blah.com", onMount: true })
 
-    expect(config2.result.current).toEqual({ onMount: true, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
+    expect(config2).toEqual({ onMount: true, url: "http://blah.com", headers: { 'Content-Type': 'application/json' } })
   })
 
   it('should set headers', () => {
-    const config1 = renderHook(() => makeConfig({}, "http://blah.com", { headers: { 'Content-Type': 'multipart/form-data' } }))
+    const config1 = makeConfig({}, "http://blah.com", { headers: { 'Content-Type': 'multipart/form-data' } })
 
-    expect(config1.result.current).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'multipart/form-data' } })
+    expect(config1).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'multipart/form-data' } })
 
-    const config2 = renderHook(() => makeConfig({}, { url: "http://blah.com", onMount: false, headers: { 'Content-Type': 'multipart/form-data' } }))
+    const config2 = makeConfig({}, { url: "http://blah.com", onMount: false, headers: { 'Content-Type': 'multipart/form-data' } })
 
-    expect(config2.result.current).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'multipart/form-data' } })
+    expect(config2).toEqual({ onMount: false, url: "http://blah.com", headers: { 'Content-Type': 'multipart/form-data' } })
   });
 });
