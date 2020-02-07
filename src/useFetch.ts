1b787a19bd61c8f353d9cdce36dc67fd59d6d78b
Alex Cory <alexcoryregister@gmail.com>
2020-02-06 19:49:43 -0800

    Caching and error fix (#171)
    * v0.2.0
    
    * v0.2.1
    
    * v0.2.2
    
    * v0.2.3
    
    * v0.2.4
    
    * v0.2.5
    
    * adding `cache-first` caching, `cacheLife` option, and fixed errors to be added on !response.ok
    
    * adding docs for website
    
    * adding ad for ava and fixing docs
    
    * fixing indentation to spaces
    
    * fixing indentation
    
    * fixing indentation

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState, useCallback, useRef } from 'react'
+import useSSR from 'use-ssr'
 import {
   HTTPMethod,
   UseFetch,
@@ -7,13 +8,15 @@ import {
   Res,
   UseFetchArrayReturn,
   UseFetchObjectReturn,
-  UseFetchArgs
+  UseFetchArgs,
+  CachePolicies,
 } from './types'
 import { BodyOnly, FetchData, NoArgs } from './types'
 import useFetchArgs from './useFetchArgs'
-import useSSR from 'use-ssr'
-import makeRouteAndOptions from './makeRouteAndOptions'
+import doFetchArgs from './doFetchArgs'
 import { isEmpty, invariant } from './utils'
+
+const { CACHE_FIRST } = CachePolicies
 
 const responseMethods = ['clone', 'error', 'redirect', 'arrayBuffer', 'blob', 'formData', 'json', 'text']
 
@@ -24,10 +27,13 @@ const makeResponseProxy = (res = {}) => new Proxy(res, {
   }
 })
 
+const cache = new Map()
+
+
 function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
   const { customOptions, requestInit, defaults, dependencies } = useFetchArgs(...args)
   const {
-    url,
+    url: initialURL,
     path,
     interceptors,
     timeout,
@@ -35,6 +41,8 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
     onTimeout,
     onAbort,
     onNewData,
+    cachePolicy, // 'cache-first' by default
+    cacheLife,
   } = customOptions
 
   const { isServer } = useSSR()
@@ -44,9 +52,9 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
   const data = useRef<TData>(defaults.data)
   const timedout = useRef(false)
   const attempts = useRef(retries)
+  const error = useRef<any>()
 
   const [loading, setLoading] = useState<boolean>(defaults.loading)
-  const [error, setError] = useState<any>()
 
   const makeFetch = useCallback((method: HTTPMethod): FetchData => {
     
@@ -59,12 +67,9 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
       controller.current.signal.onabort = onAbort
       const theController = controller.current
 
-      setLoading(true)
-      setError(undefined)
-
-      let { route, options } = await makeRouteAndOptions(
+      let { url, options, requestID } = await doFetchArgs(
         requestInit,
-        url,
+        initialURL,
         path,
         method,
         theController,
@@ -72,6 +77,22 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
         body,
         interceptors.request,
       )
+
+      const isCached = cache.has(requestID)
+      const cachedData = cache.get(requestID)
+      if (isCached && cachePolicy === CACHE_FIRST) {
+        const whenCached = cache.get(requestID + ':ts')
+        let age = Date.now() - whenCached
+        if (cacheLife > 0 && age > cacheLife) {
+          cache.delete(requestID)
+          cache.delete(requestID + ':ts')
+        } else {
+          return cachedData
+        }
+      }
+
+      setLoading(true)
+      error.current = undefined
 
       const timer = timeout > 0 && setTimeout(() => {
         timedout.current = true;
@@ -83,13 +104,15 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
       let theRes
 
       try {
-        theRes = ((await fetch(`${url}${path}${route}`, options)) || {}) as Res<TData>
+        theRes = ((await fetch(url, options)) || {}) as Res<TData>
         res.current = theRes.clone()
 
         try {
           newData = await theRes.json()
-        } catch (err) {
-          newData = (await theRes.text()) as any // FIXME: should not be `any` type
+        } catch (er) {
+          try {
+            newData = (await theRes.text()) as any // FIXME: should not be `any` type
+          } catch (er) {}
         }
 
         newData = (defaults.data && isEmpty(newData)) ? defaults.data : newData
@@ -99,12 +122,18 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
         invariant('data' in res.current, 'You must have `data` field on the Response returned from your `interceptors.response`')
         data.current = res.current.data as TData
 
+        if (cachePolicy === CACHE_FIRST) {
+          cache.set(requestID, data.current)
+          if (cacheLife > 0) cache.set(requestID + ':ts', Date.now())
+        }
+
       } catch (err) {
         if (attempts.current > 0) return doFetch(routeOrBody, body)
-        if (attempts.current < 1 && timedout.current) setError({ name: 'AbortError', message: 'Timeout Error' })
-        if (err.name !== 'AbortError') setError(err)
+        if (attempts.current < 1 && timedout.current) error.current = { name: 'AbortError', message: 'Timeout Error' }
+        if (err.name !== 'AbortError') error.current = err
 
       } finally {
+        if (theRes && !theRes.ok && !error.current) error.current = { name: theRes.status, message: theRes.statusText }
         if (attempts.current > 0) attempts.current -= 1
         timedout.current = false
         if (timer) clearTimeout(timer)
@@ -116,7 +145,7 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
 
     return doFetch
 
-  }, [url, requestInit, isServer])
+  }, [initialURL, requestInit, isServer])
 
   const post = useCallback(makeFetch(HTTPMethod.POST), [makeFetch])
   const del = useCallback(makeFetch(HTTPMethod.DELETE), [makeFetch])
@@ -132,7 +161,7 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
     query: (query, variables) => post({ query, variables }),
     mutate: (mutation, variables) => post({ mutation, variables }),
     loading: loading,
-    error,
+    error: error.current,
     data: data.current,
   }
 
@@ -156,7 +185,7 @@ function useFetch<TData = any>(...args: UseFetchArgs): UseFetch<TData> {
   useEffect(() => request.abort, [])
 
   return Object.assign<UseFetchArrayReturn<TData>, UseFetchObjectReturn<TData>>(
-    [request, makeResponseProxy(res), loading, error],
+    [request, makeResponseProxy(res), loading, error.current],
     { request, response: makeResponseProxy(res), ...request },
   )
 }
