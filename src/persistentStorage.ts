efb30c1839f84207dc2b40a184a22c3aa3908272
Mattias Rost <rrostt@gmail.com>
2020-03-12 18:54:43 +0100

    Add error handling on persistentStorage.

--- src/persistentStorage.ts
+++ src/persistentStorage.ts
@@ -1,17 +1,26 @@
 const cacheName = 'useHTTPcache'
 
+const getCache = () => {
+  try {
+    return JSON.parse(localStorage[cacheName] || '{}')
+  } catch (err) {
+    localStorage.removeItem(cacheName)
+    return {}
+  }
+}
+
 const hasPersistentData = (url: string): boolean => {
-  const urlCache = JSON.parse(localStorage[cacheName] || '{}')
+  const urlCache = getCache()
   return urlCache[url] && urlCache[url].timestamp > Date.now() - urlCache[url].ttl
 }
 
 const getPersistentData = (url: string): any => {
-  const urlCache = JSON.parse(localStorage[cacheName] || '{}')
+  const urlCache = getCache()
   return urlCache[url] && urlCache[url].data
 }
 
 const setPersistentData = (url: string, data: any, ttl = 24 * 3600000): void => {
-  const urlCache = JSON.parse(localStorage[cacheName] || '{}')
+  const urlCache = getCache()
   urlCache[url] = {
     url,
     data,
