40738296b03caa53686e31bf02a1803b4cfbed59
Alex Cory <results@alexcory.com>
2020-03-15 10:03:28 -0700

    fixing state for error boundary

--- src/ErrorBoundary.ts
+++ src/ErrorBoundary.ts
@@ -1,7 +1,12 @@
 import { Component, ReactNode } from 'react'
 
+type ErrorBoundaryState = {
+  hasError: boolean
+  error: Error | null
+}
+
 // Error boundaries currently have to be classes.
-export default class ErrorBoundary extends Component<{ fallback: NonNullable<ReactNode>|null }> {
+export default class ErrorBoundary extends Component<{ fallback: NonNullable<ReactNode>|null }, ErrorBoundaryState> {
   state = { hasError: false, error: null }
   static getDerivedStateFromError(error: Record<string, any>) {
     return {
